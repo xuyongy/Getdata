c5c9da75c80249ecd999b983da837527578e8ee3
xuyongy <49633269+xuyongy@users.noreply.github.com>
2020-01-20 10:20:05 +0800

    R 代码

--- 卡方检验.R
+++ 卡方检验.R
@@ -1,9 +1,9 @@
 matrix(c(15,23,24,12,12,21),nc=2)->c1  #创建二维表c1，nc=列数
-chisq.test(c1,correct=TRUE) #correct卡方校正
+chisq.test(c1,correct=TRUE)     #correct卡方校正
 
 matrix(c(22,10,32,1),nc=2)->c2  #创建二维表c1，nc=列数
-chisq.test(c2,correct=TRUE) #correct卡方校正
-fisher.test(c2) #确切概率
+chisq.test(c2,correct=TRUE)     #correct卡方校正
+fisher.test(c2)      #确切概率
 
 matrix(c(34,32,27,20,21,26),nc=2)->c3
 c3
